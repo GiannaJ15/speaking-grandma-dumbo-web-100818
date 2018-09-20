f389105ff888ed061c53515e48deb7a0c415711e
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-20 01:33:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@
 def speak_to_grandma(string)
   if string == string.upcase 
     return "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
   else return "HUH?! SPEAK UP, SONNY!"
     end
   end
