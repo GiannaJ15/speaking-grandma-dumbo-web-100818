fbf869f11375df967c1f63ba83c8c42ad53225ff
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-20 01:18:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,6 @@ def speak_to_grandma (string)
     puts "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else puts "HUH?! SPEAK UP, SONNY!"
-  
+  else 
+    puts "HUH?! SPEAK UP, SONNY!"
+end
