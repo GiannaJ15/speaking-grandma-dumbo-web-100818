267e184de57732559520fbb59a31f182d91c48aa
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-20 01:24:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,11 +12,4 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma (string)
-  if string == string.upcase 
-    "NO, NOT SINCE 1938!"
-  if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  if string == string.downcase 
-    return "HUH?! SPEAK UP, SONNY!"
-end
+
